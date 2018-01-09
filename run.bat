47fda61631963c8a4dae1f581a146abc6afaae76
PapyrusThePlant <papyrustheplant@gmail.com>
2018-01-09 16:39:03 +0100

    Add Python version and Pipenv checks to run scripts.

--- run.bat
+++ run.bat
@@ -1,7 +1,28 @@
 @echo off
+chcp 65001 > NUL
 
 set PIPENV_VENV_IN_PROJECT=1
-if not exist ".venv" ( pipenv install )
+
+python --version > NUL 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Cannot find Python executable, make sure it is installed and added to your PATH.
+    pause
+    exit /B 0
+)
+
+python -c "import pipenv" > NUL 2>&1
+if %ERRORLEVEL% neq 0 (
+    python -m pip install --user pipenv
+)
+
+pipenv --version > NUL 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Adding pipenv to PATH...
+    for /F %%p IN ('python -m site --user-site') do set path_pipenv=%%p
+    set PATH=%PATH%;%path_pipenv:site-packages=Scripts%
+)
+
+if not exist ".venv" ( pipenv --bare install )
 
 REM Do updates
 if "%1"=="update" (
